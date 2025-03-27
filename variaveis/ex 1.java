df882356ec62a1984d1eeaa90ef8928cfd52f4d3
pereirablaze0@gmail.com <nickolasns2008@gmail.com>
2025-03-27 14:13:26 -0300

    Atividade em java de variaveis

---
+++ variaveis/ex 1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class EX1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic her
+
+        int x = 65;
+        int y = 70;
+        int soma = x + y;
+        int subt = x - y;
+        int mult = x * y;
+        float div = x/y;
+       System.out.println(soma);
+       System.out.println(subt);
+       System.out.println(mult);
+       System.out.println(div);
+    }
+}
+    
+    
+    
+    
+
